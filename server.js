fe24e9e0d8ae95430a58635b9a977d516f3501f6
yelgon <yelgon36@gmail.com>
2020-04-14 15:32:14 -0400

    heroku

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-let express = require('express')
-let app = express()
-let reloadMagic = require('./reload-magic.js')
+let express = require("express");
+let app = express();
+let reloadMagic = require("./reload-magic.js");
 
-reloadMagic(app)
+reloadMagic(app);
 
-app.use('/', express.static('build')); // Needed for the HTML and JS files
-app.use('/', express.static('public')); // Needed for local assets
+app.use("/", express.static("build")); // Needed for the HTML and JS files
+app.use("/", express.static("public")); // Needed for local assets
 
 // Your endpoints go after this line
 
 // Your endpoints go before this line
 
-app.all('/*', (req, res, next) => { // needed for react router
-    res.sendFile(__dirname + '/build/index.html');
-})
+app.all("/*", (req, res, next) => {
+  // needed for react router
+  res.sendFile(__dirname + "/build/index.html");
+});
 
-
-app.listen(4000, '0.0.0.0', () => { console.log("Server running on port 4000") })
+const { PORT = 4000, LOCAL_ADDRESS = "0.0.0.0" } = process.env;
+app.listen(PORT, LOCAL_ADDRESS, () => {
+  console.log("app running on port" + PORT);
+});
