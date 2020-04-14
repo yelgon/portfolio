a71afecd3588bf0a7aba45db548213af00b2ed26
yelgon <yelgon36@gmail.com>
2020-04-14 15:04:25 -0400

    portfolio done

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -16,6 +16,21 @@ const Wrapper = styled.div`
     text-decoration: none;
     font-family: cursive;
   }
+  .work {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-content: space-between;
+    align-items: center;
+    justify-items: center;
+    border: 2px solid black;
+    border-radius: 1em;
+    margin: 10px;
+    height: 250px;
+    background: #d4edf4;
+    li {
+      padding: 5px;
+    }
+  }
 `;
 class Experience extends Component {
   render() {
@@ -25,6 +40,38 @@ class Experience extends Component {
           <Link to="/" className="home">
             Home
           </Link>
+        </div>
+        <div className="work">
+          <h2>Industrial Draftman</h2>
+          <h4>2016.12-2020.04</h4>
+          <h3>Mr.Roller ( Montreal )</h3>
+          <div>
+            <ul>
+              <li>Mechanical design using AutoCAD and ProE</li>
+            </ul>
+          </div>
+        </div>
+        <div className="work">
+          <h2>Math Teacher</h2>
+          <h4>2011.01-2015.01</h4>
+          <h3>Gubei Academy ( Shanghai, China )</h3>
+          <div>
+            <ul>
+              <li>Teaching Math ( Calculus 1,2 and Linear Algebra )</li>
+              <li>Supporting communication between teacher and parents</li>
+            </ul>
+          </div>
+        </div>
+        <div className="work">
+          <h2>Team Leader in Math department </h2>
+          <h4>2006.02-2012.03</h4>
+          <h3>RootM ( Seoul, S.Korea )</h3>
+          <div>
+            <ul>
+              <li>Teaching Math ( Calculus 1,2 and Geometry )</li>
+              <li>Training how to teach Math for other teachers </li>
+            </ul>
+          </div>
         </div>
       </Wrapper>
     );
